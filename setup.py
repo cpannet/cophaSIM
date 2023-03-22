ae9c011c4a7de52d98b18554f7f775867870aceb
Cyril Pannetier <cyril@cpannetier.fr>
2023-03-22 17:06:43 +0100

    nom temporaire

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name='cophaSIM',
+setup(name='cophasim',
       version='0.1',
       description='Fringe tracking simulator',
       url='https://gitlab.oca.eu/cpannetier/cophaSIM.git',
       author='Cyril Pannetier',
-      author_email='cyril@cpannetier.fr',
+      author_email='contact@cpannetier.fr',
       license='',
       install_requires=['matplotlib','numpy','scipy','astropy','pandas','sympy',
                         'tabulate','bode_utils','joblib','psutil'],
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
-      package_data = {'cophaSIM':['data/interferometers/*.fits',
+      package_data = {'cophasim':['data/interferometers/*.fits',
                                    'data/observations/CHARA/Unresolved/*.fits',
                                    'data/observations/CHARA/1mas/*.fits',
                                    'data/observations/CHARA/2mas/*.fits']})
